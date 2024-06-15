5597f515f6b389f6636b2ac226121d25b5794794
onoyima <clintonfaze@outlook.com>
2024-06-15 14:21:02 +0100

    updateds

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -17,7 +17,7 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ title }) => {
+const Project = ({ title, description }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
     bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
