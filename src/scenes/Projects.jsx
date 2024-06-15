c90925a7bafda397cdca95f8aa33daa165abca55
onoyima <clintonfaze@outlook.com>
2024-06-15 14:10:47 +0100

    updated

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,3 +1,5 @@
+
+
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 
@@ -24,9 +26,7 @@ const Project = ({ title }) => {
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">
-        "Revolutionizing possibilities with groundbreaking software innovation. Introducing our latest masterpiece, a game-changing software that simplifies complexities and elevates efficiency. Experience the power of technological advancement..
-        </p>
+        <p className="mt-7">{description}</p>
       </div>
       <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
     </motion.div>
@@ -57,7 +57,7 @@ const Projects = () => {
           </div>
         </div>
         <p className="mt-10 mb-10">
-        Building the digital future, one line of code at a time. Proud of the seamless software solution created with passion, precision, and innovation. 
+        Building the digital future, one line of code at a time. Proud of the seamless software-solution created with passion, precision, and innovation. 
         </p>
       </motion.div>
 
@@ -77,17 +77,17 @@ const Projects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" />
-          <Project title="Project 2" />
+          <Project title="Project 1" description="Data Anylse Dashboard"/>
+          <Project title="Project 2" description="Veritas University Students Portal" />
 
           {/* ROW 2 */}
-          <Project title="Project 3" />
-          <Project title="Project 4" />
-          <Project title="Project 5" />
+          <Project title="Project 3" description="Porfolio Project" />
+          <Project title="Project 4" description="Blog and News Website" />
+          <Project title="Project 5" description="E-commerce site full app" />
 
           {/* ROW 3 */}
-          <Project title="Project 6" />
-          <Project title="Project 7" />
+          <Project title="Project 6"  description="Concert Booker site"/>
+          <Project title="Project 7" description="Food delivery Website" />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
--- src/scenes/projects1.jsx
+++ src/scenes/projects1.jsx
@@ -24,7 +24,9 @@ const Project = ({ title }) => {
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">{description}</p>
+        <p className="mt-7">
+        "Revolutionizing possibilities with groundbreaking  software innovation. Introducing our latest masterpiece, a game-changing software that simplifies complexities and elevates efficiency. Experience the power of technological advancement..
+        </p>
       </div>
       <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
     </motion.div>
@@ -75,17 +77,17 @@ const Projects = () => {
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" description="Data Anylse Dashboard"/>
-          <Project title="Project 2" description="Veritas University Students Portal" />
+          <Project title="Project 1" />
+          <Project title="Project 2" />
 
           {/* ROW 2 */}
-          <Project title="Project 3" description="Porfolio Project" />
-          <Project title="Project 4" description="Blog and News Website" />
-          <Project title="Project 5" description="E-commerce site full app" />
+          <Project title="Project 3" />
+          <Project title="Project 4" />
+          <Project title="Project 5" />
 
           {/* ROW 3 */}
-          <Project title="Project 6"  description="Concert Booker site"/>
-          <Project title="Project 7" description="Food delivery Website" />
+          <Project title="Project 6" />
+          <Project title="Project 7" />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
@@ -99,3 +101,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
